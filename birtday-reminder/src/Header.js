90565d9622bd38f3da0e685d6766b68e4c16ade6
grumpy-cat <grumpy-cat@mac>
2020-12-31 16:38:29 +0100

    Add a basic Header

---
+++ birtday-reminder/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <header>
+      <h1>Birthday Reminder</h1>
+    </header>
+  );
+}
+
+export default Header;
