f6544051aa41a30482b9ba8cc20eb3ca19441c1b
grumpy-cat <grumpy-cat@mac>
2020-12-31 16:40:45 +0100

    Provide a basic Persons Component:
    - Holds n Person Components
    - Card Style
    Provide a basic Persons Component:
    
    - Holds n Person Components
    - Card Style

---
+++ birtday-reminder/src/Persons.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Person from "./Person";
+import "./Persons.css";
+import { data } from "./data";
+
+function Persons() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+
+  today = mm + "-" + dd;
+  let todaysBirthday = data.filter(
+    (person) => person.birthday.slice(5, 10) === today
+  );
+
+  return (
+    <main>
+      <section className="personsContainer">
+        {todaysBirthday.map((person) => {
+          return <Person {...person}></Person>;
+        })}
+        <button type="button" className="btn">
+          Change Date
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default Persons;
