9c7e28fd38a338032013418c30500da364687187
grumpy-cat <grumpy-cat@mac>
2020-12-31 16:43:32 +0100

    Provide some dummy like data

---
+++ birtday-reminder/src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+  { key: 1, name: "John Miller", birthday: "2000-12-30" },
+  { key: 2, name: "Tom Grill", birthday: "1992-12-31" },
+  { key: 3, name: "Susan Bay", birthday: "1988-12-30" },
+  { key: 4, name: "Anna Montana", birthday: "1992-12-31" },
+  { key: 5, name: "Alfred Quak", birthday: "1982-12-31" },
+  { key: 6, name: "Hans Meier", birthday: "1986-12-31" },
+  { key: 7, name: "Mercedes Benz", birthday: "1978-12-31" },
+  { key: 8, name: "Georg Lucas", birthday: "1998-12-31" },
+  { key: 9, name: "Andrew Blue", birthday: "2000-12-31" },
+  { key: 10, name: "Mohamed Lee", birthday: "1991-12-31" },
+];
