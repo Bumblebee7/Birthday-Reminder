671e0cff8add1cea0f80e00322ad8db3a4be6e86
grumpy-cat <grumpy-cat@mac>
2020-12-31 16:38:55 +0100

    Add a basic Footer

---
+++ birtday-reminder/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p> The floor is lava!</p>
+    </footer>
+  );
+}
+
+export default Footer;
