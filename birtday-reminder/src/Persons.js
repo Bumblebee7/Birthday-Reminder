69387c88c82b79125cebeba79b7ff08b6aee2963
grumpy-cat <grumpy-cat@mac>
2021-01-01 23:07:23 +0100

    Display image from data

--- birtday-reminder/src/Persons.js
+++ birtday-reminder/src/Persons.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Person from "./Person";
 import "./Persons.css";
 import { data } from "./data";
@@ -7,21 +7,27 @@ function Persons() {
   let today = new Date();
   let dd = String(today.getDate()).padStart(2, "0");
   let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let todayDDMM = mm + "-" + dd;
 
-  today = mm + "-" + dd;
-  let todaysBirthday = data.filter(
-    (person) => person.birthday.slice(5, 10) === today
+  const [personsList, setPersonsList] = useState(data);
+  let todaysBirthday = personsList.filter(
+    (person) => person.birthday.slice(5, 10) === todayDDMM
   );
 
   return (
     <main>
       <section className="personsContainer">
+        <h3>{todaysBirthday.length} Birthdays today</h3>
+
         {todaysBirthday.map((person) => {
           return <Person {...person}></Person>;
         })}
-        <input type="date" id="dateField"></input>
-        <button type="button" className="btn">
-          Change Date
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setPersonsList([])}
+        >
+          Delete all Entries!
         </button>
       </section>
     </main>
